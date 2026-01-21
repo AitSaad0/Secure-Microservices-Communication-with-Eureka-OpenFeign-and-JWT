87e561db371f2a1d07c90ef4811a602906e0d334
Ait_SaaD <saadaitabd2004@gmail.com>
2026-01-21 10:47:43 +0100

    first commit

---
+++ registry-service/src/test/java/com/ctf_projet/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctf_projet.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
